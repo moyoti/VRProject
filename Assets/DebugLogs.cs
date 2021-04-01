21d3eeab6710476f7e9a6cbf25e9d40325ac1a0d
moyoti <q994308383@gmail.com>
2021-04-01 20:06:33 +0800

    init

---
+++ Assets/DebugLogs.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DebugLogs : MonoBehaviour
+{
+    public GameObject TextGO;
+    public GameObject LA;
+    public GameObject RA;
+    private TextMeshPro text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = TextGO.GetComponent<TextMeshPro>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string texts = "";
+        texts += "LA Pos:" + LA.transform.position.ToString();
+        texts += "LA Rot:" + LA.transform.rotation.ToString();
+        texts += "RA Pos:" + RA.transform.position.ToString();
+        texts += "RA Rot:" + RA.transform.rotation.ToString();
+        text.text = texts;
+    }
+}
